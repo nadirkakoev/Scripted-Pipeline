5c70ecbdefb2787ace22abef9d0e5f1f4afdf809
nadirkakoev <nadir_kakoev@yahoo.com>
2020-08-30 03:10:43 +0000

    prepare-worker.changed with credetials

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,5 +1,10 @@
 node {
-    stage('Init') {
-        sh 'hostname'
+    withCredentials([sshUserPrivateKey(credentialsId: 'f4a82031-e1d8-475f-8019-95e76f844d28', 
+    keyFileVariable: 'sshkey', passphraseVariable: '',
+    usernameVariable: 'sshusername')]) {
+        stage('Init') {
+            sh ' ssh -o StrictHostKeyChecking=no -i $sshkey   $sshusername@157.245.89.156 yum install epel-release  -y'
+        }
     }
 }
+
