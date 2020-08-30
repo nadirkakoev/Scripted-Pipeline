05d9011117f1b8e3d131987fad4a1f7be04b0ba3
nadirkakoev <nadir_kakoev@yahoo.com>
2020-08-30 05:27:37 +0000

    test55

---
+++ test.groovy
@@ -0,0 +1,25 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Please Inter VM IP!!!',  name: 'nodeIP', trim: true)
+        ])
+    ])
+
+if (nodeIP?.trim()) {
+        node {
+            withCredentials([sshUserPrivateKey(credentialsId: 'f4a82031-e1d8-475f-8019-95e76f844d28',  keyFileVariable: 'sshkey', passphraseVariable: '', usernameVariable: 'sshusername')]) {
+                stage('Init') {
+                    sh ' ssh -o StrictHostKeyChecking=no -i $sshkey  $sshusername@${nodeIP} yum install epel-release  -y'
+                }
+                stage('Install git') {
+                    sh ' ssh -o StrictHostKeyChecking=no -i $sshkey  $sshusername@${nodeIP} yum install git  -y'
+                }
+                stage('Install Java') {
+                    sh ' ssh -o StrictHostKeyChecking=no -i $sshkey  $sshusername@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
+            }
+        }
+    }
+} 
+else {
+   error 'Please enter valid ip address!!'
+}
+
